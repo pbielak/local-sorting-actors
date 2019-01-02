f60894f6723d541ed55d5ed290689c3300146cef
Piotr Bielak <piotrbielak2@gmail.com>
2019-01-02 12:02:34 +0100

    Version with args

--- src/main.rs
+++ src/main.rs
@@ -3,83 +3,71 @@ extern crate futures;
 extern crate env_logger;
 #[macro_use] extern crate log;
 extern crate rand;
+#[macro_use] extern crate structopt;
 extern crate time;
 
 use actix::prelude::*;
 use futures::{future, Future};
+use structopt::StructOpt;
+use std::path::PathBuf;
 
+mod args;
 mod messages;
-mod supervisor;
 mod sorting_actor;
+mod supervisor;
 mod util;
 
 
-fn run_default() {
-    std::env::set_var("RUST_LOG", "debug");
-    env_logger::init();
+fn run_system(args: args::CliArgs) {
+    let input_path: PathBuf = args.input;
+    let n: usize = args.n;
+    let k: usize = args.k;
+    let output_path: PathBuf = args.output;
 
-    const P: usize = 90;//9_000_000; // Array size
-    const N: usize = 5; // Number of actors
-    const K: usize = 3; // Number of chunks
-
-    let numbers = util::random_vec(P);
-    let sort_req = messages::SortingRequest::new(numbers);
-
-
-    System::run(|| {
-        let supervisor = Arbiter::start(|_| supervisor::SupervisorActor::new("MASTER", N, K));
-        let res = supervisor.send(sort_req);
-
-        Arbiter::spawn(
-            res.then(|r| {
-                match r {
-                    Ok(r) => info!("SortingResponse[Vec[{:?}], Duration: {} (ms)]", r.values.len(), r.duration),
-                    _ => error!("Error occurred!")
-                }
-
-                System::current().stop();
-                future::result(Ok(()))
-            })
-        );
-    });
-}
-
-
-fn get_time(numbers: &Vec<i64>, n: usize, k: usize) -> i64 {
-    let _sys = System::new("Sorting system");
-    let sort_req = messages::SortingRequest::new(numbers.clone());
+    let system = System::new("Sorting system");
 
     let supervisor = Arbiter::start(move |_| supervisor::SupervisorActor::new("MASTER", n, k));
+
+    let numbers: Vec<i64> = util::read_numbers(input_path).unwrap();
+    debug!("Done reading numbers: Vec[{}]", numbers.len());
+    let sort_req = messages::SortingRequest::new(numbers);
     let res = supervisor.send(sort_req);
 
-    res.wait().unwrap().duration
+    Arbiter::spawn(
+        res.then(|r| {
+            match r {
+                Ok(r) => {
+                    debug!("SortingResponse[Vec[{:?}], Duration: {} (ms)]", r.values.len(), r.duration);
+                    let write_ok = util::write_numbers(output_path, &r.values).unwrap();
+                    debug!("Write ok: {:?}", write_ok);
+
+                    println!("{}", r.duration);
+                },
+                _ => error!("Error occurred!")
+            }
+
+            System::current().stop();
+            future::result(Ok(()))
+        })
+    );
+
+    system.run();
 }
 
 
-fn measure_times() {
-    std::env::set_var("RUST_LOG", "info");
+fn setup_logger(args: &args::CliArgs) {
+    let log_level = match args.debug {
+        true => "debug",
+        false => "info"
+    };
+
+    std::env::set_var("RUST_LOG", log_level);
     env_logger::init();
-
-    const P: usize = 900_000; // Array size
-    let numbers = util::random_vec(P);
-
-    const MAX_N: usize = 10;
-    const MAX_K: usize = 10;
-
-    let mut results: Vec<(usize, usize, i64)> = vec![];
-
-    for n in 1..=MAX_N {
-        for k in 1..=MAX_K {
-            info!("N: {} K: {}", n, k);
-            results.push((n, k, get_time(&numbers, n, k)));
-        }
-    }
-
-    info!("Results: {:?}", results);
 }
 
 
 fn main() {
-    run_default()
-    //measure_times()
+    let args = args::CliArgs::from_args();
+    setup_logger(&args);
+    run_system(args);
 }
--- src/util.rs
+++ src/util.rs
@@ -1,18 +1,10 @@
 /*
-Util functions for data generation, time measurement etc.
+Util functions
 */
-use rand::prelude::*;
+use std::io::{BufRead, BufReader, Error, ErrorKind, Write};
+use std::fs::File;
+use std::path::PathBuf;
 use time::PreciseTime;
-
-
-pub fn random_vec(n: usize) -> Vec<i64> {
-    let mut rng = rand::thread_rng();
-    let random_numbers: Vec<i64> = (0..n).map(|_| {
-        rng.gen_range(0, n as i64)
-    }).collect();
-
-    random_numbers
-}
 
 
 pub fn measure_time<T, R>(f: &Fn(T) -> R, arg: T) -> (R, i64) {
@@ -23,4 +15,35 @@ pub fn measure_time<T, R>(f: &Fn(T) -> R, arg: T) -> (R, i64) {
     let duration = start.to(end).num_milliseconds();
 
     (result, duration)
+}
+
+
+pub fn read_numbers(path: PathBuf) -> Result<Vec<i64>, Error> {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+
+    reader
+        .lines()
+        .map(|line| {
+            line.and_then(|v| {
+                v
+                    .parse()
+                    .map_err(|e| {
+                        Error::new(ErrorKind::InvalidData, e)
+                    })
+            })
+        })
+        .collect()
+}
+
+
+pub fn write_numbers(path: PathBuf, numbers: &Vec<i64>) -> Result<bool, Error> {
+    let mut file = File::create(path).unwrap();
+
+    numbers.iter().for_each(|v| {
+        file.write_all(format!("{}\n", v).as_bytes()).unwrap()
+    });
+
+
+    Ok(true)
 }
